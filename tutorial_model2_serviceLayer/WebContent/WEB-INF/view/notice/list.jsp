464b98e02d6e096234882b7dce356fa013082d74
young-jin-lee <dof0717@gmail.com>
2020-07-25 23:34:36 +0900

    service paged completed

--- tutorial_model2_serviceLayer/WebContent/WEB-INF/view/notice/list.jsp
+++ tutorial_model2_serviceLayer/WebContent/WEB-INF/view/notice/list.jsp
@@ -4,7 +4,7 @@
     pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
-
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
 <!DOCTYPE html>
 <html>
 
@@ -154,11 +154,11 @@
 						<legend class="hidden">공지사항 검색 필드</legend>
 						<label class="hidden">검색분류</label>
 						<select name="f">
-							<option  value="title">제목</option>
-							<option  value="writerId">작성자</option>
+							<option ${(param.f == "title")?"selected":""} value="title">제목</option>
+							<option ${(param.f == "writer_id")?"selected":""}value="writer_id">작성자</option>
 						</select> 
 						<label class="hidden">검색어</label>
-						<input type="text" name="q" value=""/>
+						<input type="text" name="q" value="${param.q}"/>
 						<input class="btn btn-search" type="submit" value="검색" />
 					</fieldset>
 				</form>
@@ -183,10 +183,10 @@
 						pageContext.setAttribute("n", n);
 					%> --%>
 					
-					<c:forEach var="n" items="${list}" begin="0" end="4" varStatus = "st">
+					<c:forEach var="n" items="${list}" begin="0" end="10" varStatus = "st">
 					<tr>
 						<td>${st.index+1} / ${n.id}</td>
-						<td class="title indent text-align-left"><a href="detail?id=${n.id}">${n.title}</a></td>
+						<td class="title indent text-align-left"><a href="detail?id=${n.id}">${n.title}</a><span> [${n.cmtCount}]</span></td>
 						<td>${n.writerId}</td>
 						<td><fmt:formatDate pattern="yyyy년-MM월-dd일 " value="${n.regdate}"/></td>
 						<td>${n.hit}</td>
@@ -197,21 +197,23 @@
 				</table>
 			</div>
 			
+			<c:set var="page" value="${(empty param.p)?1:param.p}" />
+			<c:set var="startNum" value="${page-(page-1)%5}" />
+			<c:set var="lastNum" value="${fn:substringBefore(Math.ceil(count/10), '.')}" />
+					
 			<div class="indexer margin-top align-right">
 				<h3 class="hidden">현재 페이지</h3>
-				<div><span class="text-orange text-strong">1</span> / 1 pages</div>
+				<div><span class="text-orange text-strong">${(empty param.p)?1:param.p }</span> / ${lastNum} pages</div>
 			</div>
 
 			<div class="margin-top align-center pager">	
 		
 		
-	<c:set var="page" value="${(param.p == null)?1:param.p}" />
-	<c:set var="startNum" value="${page-(page-1)%5}" />
-	<c:set var="lastNum" value="23" />
+	
 	
 	<div>
 		<c:if test="${startNum>1}">
-			<a href="?p=${startNum-1}&t=&q=" class="btn btn-prev">이전</a>
+			<a href="?p=${startNum-1}&f=&q=" class="btn btn-prev">이전</a>
 		</c:if>
 		<c:if test="${startNum<=1}">
 			<span class="btn btn-prev" onclick="alert('이전 페이지가 없습니다.');">이전</span>
@@ -220,15 +222,17 @@
 
 	<ul class="-list- center">
 	<c:forEach var="i" begin="0" end="4">
-		<li><a class="-text- orange bold" href="?p=${i+startNum}&t=&q=" >${i+startNum}</a></li>
+	<c:if test="${(startNum+i) <= lastNum}">
+		<li><a class="-text- ${(page==(i+startNum))?'orange':''} bold" href="?p=${i+startNum}&f=${param.f}&q=${param.q}" >${i+startNum}</a></li>
+	</c:if>
 	</c:forEach>
 	</ul>
 	
 	<div>
-		<c:if test="${startNum+5 < lastNum}">
-			<a href="?p=${startNum+5}&t=&q=" class="btn btn-next">다음</a>
+		<c:if test="${startNum+4 < lastNum}">
+			<a href="?p=${startNum+5}&f=&q=" class="btn btn-next">다음</a>
 		</c:if>
-		<c:if test="${startNum+5 >= lastNum}">
+		<c:if test="${startNum+4 >= lastNum}">
 			<span class="btn btn-next" onclick="alert('다음 페이지가 없습니다.');">다음</span>
 		</c:if>
 	</div>
--- tutorial_model2_serviceLayer/src/com/tutorial/web/service/NoticeService.java
+++ tutorial_model2_serviceLayer/src/com/tutorial/web/service/NoticeService.java
@@ -1,29 +1,94 @@
 package com.tutorial.web.service;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import com.tutorial.web.entity.Notice;
+import com.tutorial.web.entity.NoticeView;
 
 public class NoticeService {
-	public List<Notice> getNoticeList(){
+	public List<NoticeView> getNoticeList(){
 		
 		return getNoticeList("title", "", 1);
 	}
 	
-	public List<Notice> getNoticeList(int page){
+	public List<NoticeView> getNoticeList(int page){
 			
 			return getNoticeList("title", "", page);
 	}
 	
-	public List<Notice> getNoticeList(String field, String query, int page){
+	public List<NoticeView> getNoticeList(String field, String query, int page){
+		
+		List<NoticeView> list = new ArrayList<>();
+		
+//		String sql = "SELECT * FROM (" + 
+//				" SELECT ROWNUM NUM, N.*" + 
+//				" FROM (SELECT * FROM NOTICE WHERE TITLE LIKE 2 ORDER BY REGDATE DESC) N" + 
+//				" )" + 
+//				" WHERE NUM BETWEEN 1 AND 10";
 		
 		String sql = "SELECT * FROM (" + 
 				" SELECT ROWNUM NUM, N.*" + 
-				" FROM (SELECT * FROM NOTICE ORDER BY REGDATE DESC) N" + 
+				" FROM (SELECT * FROM NOTICE_VIEW WHERE "+field+" LIKE ? ORDER BY REGDATE DESC) N" + 
 				" )" + 
-				" WHERE NUM BETWEEN 1 AND 3";
+				" WHERE NUM BETWEEN ? AND ?";
+		
+		String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection(url, "testuser", "123456789a");
+//			Statement st = con.createStatement();
+//			ResultSet rs = st.executeQuery(sql);
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setString(1, "%"+query+"%");
+			st.setInt(2, 1+(page-1)*10);
+			st.setInt(3, page*10);
+			ResultSet rs = st.executeQuery();
+
+			while(rs.next()){
+				int id = rs.getInt("ID");
+				String title = rs.getString("TITLE");
+				Date regdate = rs.getDate("REGDATE");
+				String writerId = rs.getString("WRITER_ID");
+				int hit = rs.getInt("HIT");
+				//String content = rs.getString("CONTENT");
+				String files = rs.getString("FILES");
+				int cmtCount = rs.getInt("CMT_COUNT");
 				
-		return null;
+				NoticeView notice = new NoticeView(
+						id,
+						title,
+						regdate,
+						writerId,
+						hit,
+						//content,
+						files,
+						cmtCount
+						);
+				list.add(notice);
+			} 
+
+			rs.close();
+			st.close();
+			con.close(); 
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+				
+		return list;
 	}
 	
 	public int getNoticeCount() {
@@ -33,30 +98,195 @@ public class NoticeService {
 	
 	public int getNoticeCount(String field, String query) {
 		
-		String sql = "SELECT * FROM (" + 
-				" SELECT ROWNUM NUM, N.*" + 
-				" FROM (SELECT * FROM NOTICE ORDER BY REGDATE DESC) N" + 
-				" )" + 
-				" WHERE NUM BETWEEN 1 AND 3";
+		int count = 0;
 		
-		return 0;
+		String sql = "SELECT COUNT(ID) COUNT FROM (" + 
+				" SELECT ROWNUM NUM, N.*" + 
+				" FROM (SELECT * FROM NOTICE WHERE "+field+" LIKE ? ORDER BY REGDATE DESC) N" + 
+				") ";
+		
+		String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection(url, "testuser", "123456789a");
+
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setString(1, "%"+query+"%");
+	
+			ResultSet rs = st.executeQuery();
+			
+			if(rs.next())
+				count = rs.getInt("count");
+
+			rs.close();
+			st.close();
+			con.close(); 
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return count;
 	}
 	
 	public Notice getNotice(int id) {
-		return null;
+		Notice notice = null;
+		
+		String sql = "SELECT * FROM NOTICE WHERE ID = ?";
+		
+		String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection(url, "testuser", "123456789a");
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setInt(1, id);
+
+			ResultSet rs = st.executeQuery();
+
+			if(rs.next()){
+				int nid = rs.getInt("ID");
+				String title = rs.getString("TITLE");
+				Date regdate = rs.getDate("REGDATE");
+				String writerId = rs.getString("WRITER_ID");
+				int hit = rs.getInt("HIT");
+				String content = rs.getString("CONTENT");
+				String files = rs.getString("FILES");
+				
+				
+				notice = new Notice(
+						nid,
+						title,
+						regdate,
+						writerId,
+						hit,
+						content,
+						files
+						);
+			} 
+
+			rs.close();
+			st.close();
+			con.close(); 
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return notice;
 	}
 	public Notice getNextNotice(int id) {
-		
+		Notice notice = null;
 		String sql = "SELECT * FROM NOTICE" + 
 				"WHERE ID = (" + 
 				"    SELECT ID FROM NOTICE " + 
-				"    WHERE REGDATE > (SELECT REGDATE FROM NOTICE WHERE ID = 3)" + 
+				"    WHERE REGDATE > (SELECT REGDATE FROM NOTICE WHERE ID = ?)" + 
 				"    AND ROWNUM = 1" + 
 				")";
 		
-		return null;
+		String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection(url, "testuser", "123456789a");
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setInt(1, id);
+
+			ResultSet rs = st.executeQuery();
+
+			if(rs.next()){
+				int nid = rs.getInt("ID");
+				String title = rs.getString("TITLE");
+				Date regdate = rs.getDate("REGDATE");
+				String writerId = rs.getString("WRITER_ID");
+				int hit = rs.getInt("HIT");
+				String content = rs.getString("CONTENT");
+				String files = rs.getString("FILES");
+				
+				
+				notice = new Notice(
+						nid,
+						title,
+						regdate,
+						writerId,
+						hit,
+						content,
+						files
+						);
+			} 
+
+			rs.close();
+			st.close();
+			con.close(); 
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return notice;
 	}
 	public Notice getPrevNotice(int id) {
-		return null;
+		Notice notice = null;
+		
+		String sql = "SELECT * FROM NOTICE" + 
+				"WHERE ID = (" + 
+				"    SELECT ID FROM (SELECT * FROM NOTICE ORDER BY REGDATE DESC)  " + 
+				"    WHERE REGDATE > (SELECT REGDATE FROM NOTICE WHERE ID = ?)" + 
+				"    AND ROWNUM = 1" + 
+				")";
+		
+		String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection(url, "testuser", "123456789a");
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setInt(1, id);
+
+			ResultSet rs = st.executeQuery();
+
+			if(rs.next()){
+				int nid = rs.getInt("ID");
+				String title = rs.getString("TITLE");
+				Date regdate = rs.getDate("REGDATE");
+				String writerId = rs.getString("WRITER_ID");
+				int hit = rs.getInt("HIT");
+				String content = rs.getString("CONTENT");
+				String files = rs.getString("FILES");
+				
+				
+				notice = new Notice(
+						nid,
+						title,
+						regdate,
+						writerId,
+						hit,
+						content,
+						files
+						);
+			} 
+
+			rs.close();
+			st.close();
+			con.close(); 
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return notice;
 	}
 }
